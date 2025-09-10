b6d205aa589085d44c96eb35519857136da81c42
keshavsoft <nknnkumar@yahoo.com>
2025-09-10 12:51:04 +0530

    versioning started

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-import { StartFunc } from "./Projects/WA/entryFile.js";
+import { StartFunc as StartFuncPortListen } from "./PortListen.js";
 
-StartFunc().then();
+import { StartFunc as StartFuncFromEntryFile } from "./Projects/WaV1/entryFile.js";
+
+import { StartFunc as StartFuncKWSServer } from "./Projects/WebSocketServer/V2/entryFile.js";
+
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+global.__basedir = path.dirname(__filename);
+
+const app = express();
+const server = http.createServer(app);
+
+var port = normalizePort(process.env.PORT || '7019');
+
+app.use('/', express.static(path.join(path.resolve(), 'Public')));
+
+app.get('/StartWA', (req, res) => {
+    StartFuncFromEntryFile().then();
+});
+
+StartFuncKWSServer(server);
+
+server.listen(port, StartFuncPortListen);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+};
